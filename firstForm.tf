c2c7a4873b73cd2c45279831bc06051d53366a6e
Reza Ambler <reza.ambler@gmail.com>
2018-11-16 23:18:34 -0800

    add tags

--- firstForm.tf
+++ firstForm.tf
@@ -5,6 +5,14 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-6cd6f714"
   instance_type = "t2.nano"
+
+  tags { 
+  	Name = "Terraform Test"
+  }
+
+  volume_tags {
+  	Name = "Terraform Test"
+  }
 }
 
 resource "aws_eip" "example_ip" {
