a4d657a88d92bb889c3c102cd9b69f22e19ea836
Reza Ambler <reza.ambler@gmail.com>
2018-11-20 15:06:38 -0800

    Update vpc.tf
    Added some notes to the TF file.

--- simple-vpc/vpc.tf
+++ simple-vpc/vpc.tf
@@ -1,7 +1,14 @@
+# Define the provider, this can be one of a long list of Cloud providers
+# AWS , Azure, GCP, VMWare, etc..
+# list here : https://www.terraform.io/docs/providers/
 provider "aws" { 
   region = "us-west-2"
 }
 
+# BEGIN VARIABLES
+# Variables to be used through the TF file
+# These can also be defined in another file and passed into
+# the terraform file for consumption
 variable "aws_region" {
     description = "EC2 Region for the VPC"
     default = "us-west-2"
@@ -28,7 +35,11 @@ variable "private_subnet_cidr" {
     description = "CIDR for the Private Subnet"
     default = "192.168.200.128/25"
 }
+# END VARIABLES
 
+# Here we have multiple resource blocks. 
+# This is where the rubber meets the road, and we actually 
+# start deploying resources into the Cloud Provider (AWS)
 resource "aws_vpc" "vpc" {
   cidr_block       = "${var.vpc_cidr}"
   enable_dns_hostnames = true
@@ -36,11 +47,6 @@ resource "aws_vpc" "vpc" {
   tags {
     Name = "test vpc"
   }
-}
-
-output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = "${aws_vpc.vpc.id}"
 }
 
 resource "aws_internet_gateway" "default" {
@@ -65,19 +71,26 @@ vpc_id = "${aws_vpc.vpc.id}"
 	}
 }
 
-# //implement NAT Gateway here, not the example's nat instance
+# EIP needed for the NAT Gateway
 resource "aws_eip" "ngw-eip" {
 	vpc = true
+	# VPC ID ?
 }
 
+# //implement NAT Gateway here, not the example's nat instance (deprecated)
 resource "aws_nat_gateway" "ngw" {
 	allocation_id  = "${aws_eip.ngw-eip.id}"
 	subnet_id      = "${aws_subnet.us-west-2a-public.id}"
 }
 
+# Subnet definition, calling a variable from the begining
 resource "aws_subnet" "us-west-2a-public" {
+	# VPC ID taken from the terraform state file
+	# This ID is only available once Terraform has made the VPC
+	# Terraform handles the event ordering
 	vpc_id = "${aws_vpc.vpc.id}"
 
+	# variable defined at the top
 	cidr_block = "${var.public_subnet_cidr}"
 	availability_zone = "us-west-2a"
 
@@ -86,6 +99,7 @@ resource "aws_subnet" "us-west-2a-public" {
 	}
 }
 
+# Create a Route Table, assign it to the VPC ID
 resource "aws_route_table" "us-west-2a-public" {
 	vpc_id = "${aws_vpc.vpc.id}"
 
@@ -99,11 +113,15 @@ resource "aws_route_table" "us-west-2a-public" {
 	}
 }
 
+# Here we associate the Subnet from above with the reoute table above
+# Subnet : us-west-2a-public 
+# Route T: us-west-2a-public
 resource "aws_route_table_association" "us-west-2a-public" {
 	subnet_id = "${aws_subnet.us-west-2a-public.id}"
 	route_table_id = "${aws_route_table.us-west-2a-public.id}"
 }
 
+# Private Subnet
 resource "aws_subnet" "us-west-2a-private" {
 	vpc_id = "${aws_vpc.vpc.id}"
 
@@ -115,6 +133,7 @@ resource "aws_subnet" "us-west-2a-private" {
 	}
 }
 
+# Route table for private subnet
 resource "aws_route_table" "us-west-2a-private" {
 	vpc_id = "${aws_vpc.vpc.id}"
 
@@ -128,7 +147,19 @@ resource "aws_route_table" "us-west-2a-private" {
 	}
 }
 
+# Associate the Subnet and Route Table together
+# Subnet : us-west-2a-private
+# Route T: us-west-2a-private
+# Obviously we would have better / more generic names for some of these
+# This is just a quick example
 resource "aws_route_table_association" "us-west-2a-private" {
 	subnet_id = "${aws_subnet.us-west-2a-private.id}"
 	route_table_id = "${aws_route_table.us-west-2a-private.id}"
+}
+
+# This presents nice output to the user / consumer once
+# terraform has finished the job and has all the state information
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = "${aws_vpc.vpc.id}"
 }
