36226ed23a848d005e0a6e551ec895cd1b708abe
Reza Ambler <reza.ambler@gmail.com>
2018-11-23 21:17:55 -0800

    make example more generic

--- simple-vpc/vpc.tf
+++ simple-vpc/vpc.tf
@@ -1,9 +1,12 @@
-# Define the provider, this can be one of a long list of Cloud providers
-# AWS , Azure, GCP, VMWare, etc..
-# list here : https://www.terraform.io/docs/providers/
-provider "aws" {
-  region = "us-west-2"
-}
+#data "terraform_remote_state" "network" {
+#  backend = "s3"
+#
+#  config {
+#    bucket = "reza-terraform-state"
+#    key    = "network/terraform.tfstate"
+#    region = "us-west-2"
+#  }
+#}
 
 # BEGIN VARIABLES
 # Variables to be used through the TF file
@@ -11,14 +14,16 @@ provider "aws" {
 # the terraform file for consumption
 variable "aws_region" {
   description = "EC2 Region for the VPC"
-  default     = "us-west-2"
+  default     = "us-east-1"
 }
 
 variable "amis" {
   description = "AMIs by region"
 
   default = {
-    eu-west-1 = "ami-f1810f86" # ubuntu 14.04 LTS
+    "us-west-2" = "ami-0bb5806b2e825a199"
+    "us-east-1" = "ami-b374d5a5"
+    "eu-west-1" = "ami-f1810f86"          # ubuntu 14.04 LTS
   }
 }
 
@@ -37,7 +42,26 @@ variable "private_subnet_cidr" {
   default     = "192.168.200.128/25"
 }
 
+variable "az01" {
+  default = "us-east-1a"
+}
+
+variable "az02" {
+  default = "us-east-1b"
+}
+
+variable "az03" {
+  default = "us-east-1c"
+}
+
 # END VARIABLES
+
+# Define the provider, this can be one of a long list of Cloud providers
+# AWS , Azure, GCP, VMWare, etc..
+# list here : https://www.terraform.io/docs/providers/
+provider "aws" {
+  region = "${var.aws_region}"
+}
 
 # Here we have multiple resource blocks. 
 # This is where the rubber meets the road, and we actually 
@@ -99,7 +123,7 @@ resource "aws_eip" "ngw-eip" {
 # //implement NAT Gateway here, not the example's nat instance (deprecated)
 resource "aws_nat_gateway" "ngw" {
   allocation_id = "${aws_eip.ngw-eip.id}"
-  subnet_id     = "${aws_subnet.us-west-2a-public.id}"
+  subnet_id     = "${aws_subnet.az-01-public.id}"
 
   tags {
     Name    = "ngw"
@@ -108,8 +132,9 @@ resource "aws_nat_gateway" "ngw" {
   }
 }
 
+# "us-west-2a-public" 
 # Subnet definition, calling a variable from the begining
-resource "aws_subnet" "us-west-2a-public" {
+resource "aws_subnet" "az-01-public" {
   # VPC ID taken from the terraform state file
   # This ID is only available once Terraform has made the VPC
   # Terraform handles the event ordering
@@ -117,7 +142,7 @@ resource "aws_subnet" "us-west-2a-public" {
 
   # variable defined at the top
   cidr_block        = "${var.public_subnet_cidr}"
-  availability_zone = "us-west-2a"
+  availability_zone = "${var.az01}"
 
   tags {
     Name    = "Public Subnet Test VPC"
@@ -127,7 +152,7 @@ resource "aws_subnet" "us-west-2a-public" {
 }
 
 # Create a Route Table, assign it to the VPC ID
-resource "aws_route_table" "us-west-2a-public" {
+resource "aws_route_table" "az-01-public" {
   vpc_id = "${aws_vpc.vpc.id}"
 
   route {
@@ -145,17 +170,18 @@ resource "aws_route_table" "us-west-2a-public" {
 # Here we associate the Subnet from above with the reoute table above
 # Subnet : us-west-2a-public 
 # Route T: us-west-2a-public
-resource "aws_route_table_association" "us-west-2a-public" {
-  subnet_id      = "${aws_subnet.us-west-2a-public.id}"
-  route_table_id = "${aws_route_table.us-west-2a-public.id}"
+resource "aws_route_table_association" "az-01-public" {
+  subnet_id      = "${aws_subnet.az-01-public.id}"
+  route_table_id = "${aws_route_table.az-01-public.id}"
 }
 
+# us-west-2a-private
 # Private Subnet
-resource "aws_subnet" "us-west-2a-private" {
+resource "aws_subnet" "az-01-private" {
   vpc_id = "${aws_vpc.vpc.id}"
 
   cidr_block        = "${var.private_subnet_cidr}"
-  availability_zone = "us-west-2a"
+  availability_zone = "${var.az01}"
 
   tags {
     Name    = "Private Subnet Test VPC"
@@ -165,7 +191,7 @@ resource "aws_subnet" "us-west-2a-private" {
 }
 
 # Route table for private subnet
-resource "aws_route_table" "us-west-2a-private" {
+resource "aws_route_table" "az-01-private" {
   vpc_id = "${aws_vpc.vpc.id}"
 
   route {
@@ -185,9 +211,34 @@ resource "aws_route_table" "us-west-2a-private" {
 # Route T: us-west-2a-private
 # Obviously we would have better / more generic names for some of these
 # This is just a quick example
-resource "aws_route_table_association" "us-west-2a-private" {
-  subnet_id      = "${aws_subnet.us-west-2a-private.id}"
-  route_table_id = "${aws_route_table.us-west-2a-private.id}"
+resource "aws_route_table_association" "az-01-private" {
+  subnet_id      = "${aws_subnet.az-01-private.id}"
+  route_table_id = "${aws_route_table.az-01-private.id}"
+}
+
+resource "aws_eip" "int-001-eip" {
+  instance = "${aws_instance.proj007_instance.id}"
+}
+
+resource "aws_instance" "proj007_instance" {
+  ami           = "${lookup(var.amis, var.aws_region)}"
+  instance_type = "t2.nano"
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.proj007_instance.public_ip} > /tmp/ip_address.txt"
+  }
+
+  provisioner "local-exec" {
+    command = "sudo /usr/bin/yum install httpd;"
+  }
+
+  key_name = "Reza-East-1"
+
+  tags {
+    Name    = "ec2_instance_example"
+    Project = "PROJ007"
+    BU      = "PU"
+  }
 }
 
 # This presents nice output to the user / consumer once
